596f0e4d38c3181b169dcf0cb731e0f610cb26cb
BS\Bartosz <staniszbartosz@gmail.com>
2018-06-10 23:09:57 +0200

    Added i18n service

--- src/test/java/com/devopsbuddy/DevopsbuddyApplicationTests.java
+++ src/test/java/com/devopsbuddy/DevopsbuddyApplicationTests.java
@@ -1,16 +1,29 @@
 package com.devopsbuddy;
 
+import com.devopsbuddy.config.I18nConfig;
+import com.devopsbuddy.web.i18n.I18nService;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.MessageSource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class DevopsbuddyApplicationTests {
 
+	@Autowired
+	private I18nService i18nService;
+
 	@Test
-	public void contextLoads() {
+	public void testMessageByLocaleService(){
+		String expectedResult = "Bootstrap starter template";
+		String messageId = "index.main.callout";
+		String actual = i18nService.getMessage(messageId);
+		Assert.assertEquals("Strings don't match", expectedResult, actual);
 	}
 
 }
