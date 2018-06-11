7436e6c287f5b9865ac17e18dee690ed8978d15c
BS\Bartosz <staniszbartosz@gmail.com>
2018-06-11 18:35:22 +0200

    Added Spring Security features

---
+++ src/main/java/com/devopsbuddy/config/SecurityConfig.java
@@ -0,0 +1,43 @@
+package com.devopsbuddy.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.authentication.AuthenticationManagerFactoryBean;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    //Public URL's access
+    private static final String [] PUBLIC_MATCHERS = {
+            "/webjars/**",
+            "/css/**",
+            "/js/**",
+            "/images/**",
+            "/",
+            "/about/**",
+            "/contact/**",
+            "/error/**/*"
+    };
+
+    @Override
+    protected void configure(HttpSecurity security) throws Exception{
+        security.authorizeRequests()
+                .antMatchers(PUBLIC_MATCHERS).permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().loginPage("/login").defaultSuccessUrl("/payload")
+                .failureUrl("/login?error").permitAll()
+                .and()
+                .logout().permitAll();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder builder) throws Exception{
+        builder.inMemoryAuthentication()
+                .withUser("user").password("{noop}password")
+                .roles("USER");
+    }
+}
